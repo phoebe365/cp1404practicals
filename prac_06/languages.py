d2ef73e5db0a50f31e27a18ac4eb9e3d7d934f62
phoebe365 <phoebe.wilson@my.jcu.edu.au>
2024-11-03 19:55:54 +1000

    Add languages.py program

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -5,6 +5,7 @@ ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
 visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
 print(python)
 
+# create list
 programming_languages = [ProgrammingLanguage("Python", "Dynamic", True, 1991),
                          ProgrammingLanguage("Ruby", "Dynamic", True, 1991),
                          ProgrammingLanguage("Visual Basic", "Static", False, 1991)]
